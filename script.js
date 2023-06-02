844fb0ae70ccab9541672fc7c8cbd1aa8471970c
hugo <hugoo.ribeiro03@gmail.com>
2023-06-02 16:05:10 -0300

    versao js inicio

--- script.js
+++ script.js
@@ -1,9 +1,75 @@
-let qtd = prompt('Digite a quantidade de cartas:');
+let qtd = 0;
 
-while( qtd <4 || qtd > 14 || qtd%2===1){
-    qtd = prompt('Digite a quantidade de cartas:');
+const tipoCartas = [
+    'bobrossparrot',
+    'explodyparrot',
+    'fiestaparrot',
+    'metalparrot',
+    'revertiparrot',
+    'tripletsparrot',
+    'unicornparrot'
+]
+
+function perguntarQtdeCartas(){
+
+    qtd = Number(prompt('Digite a quantidade de cartas:'));
+
+    while( qtd <4 || qtd > 14 || qtd%2===1){
+    qtd = Number(prompt('Digite a quantidade de cartas:'));
+    }
+
+    renderizarCartas()
 }
 
+perguntarQtdeCartas()
+
+function renderizarCartas(){
+
+    const cartas = document.querySelector('.container-carta')
+    
+    for( let i = 0; i < (qtd/2); i++){
+        cartas.innerHTML += `
+        <div onclick="virarCarta(this)" class="card">
+            <div class="carta1 face">
+                <img src="back.png">
+            </div>
+            <div class="carta2 back-face face">
+                <img src="${tipoCartas[i]}.gif">
+            </div>
+        </div>`
+        cartas.innerHTML += `
+        <div onclick="virarCarta(this)" class="card">
+            <div class="carta1 face">
+                <img src="back.png">
+            </div>
+            <div class="carta2 back-face face">
+                <img src="${tipoCartas[i]}.gif">
+            </div>
+        </div>`
+    }
+    console.log(cartas);
+}
+
+
+/*
+const baralho[];
+
+let qtd = 0;
+
+function gerarBaralho(){
+
+    for( let i = 0; i < qtd/2; i++){
+        let carta = cartas [i];
+        baralho.push(carta)
+        baralho.push(carta);
+    }
+
+    baralho.sort(comparador).
+
+    distribuirCartas()
+}
+
+*/
 function virarCarta(carta) {
     const carta1 = carta.querySelector(".carta1");
     carta1.classList.toggle("front");
