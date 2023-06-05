11f0e6684aa6890d187a2a3907ba40f5675bcd15
hugo <hugoo.ribeiro03@gmail.com>
2023-06-05 10:05:57 -0300

    versao data test

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let qtd = 0;
 
-const tipoCartas = [
+const tipoCartas = [//array das imagens
     'bobrossparrot',
     'explodyparrot',
     'fiestaparrot',
@@ -10,7 +10,10 @@ const tipoCartas = [
     'unicornparrot'
 ]
 
-function perguntarQtdeCartas(){
+let baralho = [];//baralho a ser renderizado
+
+//passo1: receber a quatidade de cartas do usuario
+function perguntarQtdeCartas(){//perguntar ao usuario o numero de cartas
 
     qtd = Number(prompt('Digite a quantidade de cartas:'));
 
@@ -18,58 +21,53 @@ function perguntarQtdeCartas(){
     qtd = Number(prompt('Digite a quantidade de cartas:'));
     }
 
-    renderizarCartas()
+    adicionarBaralho();
+    
 }
 
 perguntarQtdeCartas()
 
-function renderizarCartas(){
+//passo2: guardar essa quantidade dentro de uma array
 
+function adicionarBaralho(){
+   
+    for( let i = 0; i < qtd/2; i++){//duplicando
+        let addCartas = tipoCartas[i];
+        baralho.push(addCartas);
+        baralho.push(addCartas);
+    }
+
+//passo3: emabaralhar esse array
+    baralho.sort(comparador);//embaralhando a array baralho depois de duplicar
+
+    renderizarCartas();
+    console.log(baralho);
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+//passo4: mostrar no HTML
+function renderizarCartas(){//colocando as cartas no HTML
+
+    
     const cartas = document.querySelector('.container-carta')
     
-    for( let i = 0; i < (qtd/2); i++){
+    for( let i = 0; i < qtd; i++){
         cartas.innerHTML += `
-        <div onclick="virarCarta(this)" class="card">
+        <div onclick="virarCarta(this)" class="card" data-test="card">
             <div class="carta1 face">
-                <img src="back.png">
+                <img data-test="face-down-image" src="back.png">
             </div>
             <div class="carta2 back-face face">
-                <img src="${tipoCartas[i]}.gif">
-            </div>
-        </div>`
-        cartas.innerHTML += `
-        <div onclick="virarCarta(this)" class="card">
-            <div class="carta1 face">
-                <img src="back.png">
-            </div>
-            <div class="carta2 back-face face">
-                <img src="${tipoCartas[i]}.gif">
+                <img  data-test="face-up-image" src="${baralho[i]}.gif">
             </div>
         </div>`
     }
     console.log(cartas);
 }
 
-
-/*
-const baralho[];
-
-let qtd = 0;
-
-function gerarBaralho(){
-
-    for( let i = 0; i < qtd/2; i++){
-        let carta = cartas [i];
-        baralho.push(carta)
-        baralho.push(carta);
-    }
-
-    baralho.sort(comparador).
-
-    distribuirCartas()
-}
-
-*/
 function virarCarta(carta) {
     const carta1 = carta.querySelector(".carta1");
     carta1.classList.toggle("front");
